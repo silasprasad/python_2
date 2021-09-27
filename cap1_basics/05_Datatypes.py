8b36f7936f4ae0ac72fb633de765742b3e928938
Tabitha <tabitha@Tabithas-MacBook-Air.local>
2021-09-28 00:39:12 +0530

    datatypes added

---
+++ cap1_basics/05_Datatypes.py
@@ -0,0 +1,35 @@
+
+# #List can be modified
+# list1 = [1,2,"Silas",5,9]
+# print(list1[2])
+
+# A tuple is an immutable (can’t change or modified) data type in Python.
+# tuple1 = (4,5,"prasad",10)
+# print(tuple1[2])
+
+# #Dictionary: Dictionary is a collection of key-value pairs.
+# Properties of Python Dictionaries
+# It is unordered
+# It is mutable
+# It is indexed
+# It cannot contain duplicate keys
+
+# a = {"key": "value",
+# "harry": "code",
+# "marks" : "100",
+# "list": [1,2,9]}
+# print(a["key"])	# Prints value
+# print(a["list"]) 	# Prints [1,2,9] 
+
+#Set: Set is a collection of non-repetitive elements.
+# Properties of Sets
+# Sets are unordered # Elements order doesn’t matter
+# Sets are unindexed # Cannot access elements by index
+# There is no way to change items in sets
+# Sets cannot contain duplicate values
+# We cannot add list inside a Set
+# S=Set()          # No repetition allowed!
+# S.add(1)
+# S.add(2)
+# or Set = {1,2}
+
