bab4b37230b8a6709c4d2811b1615b994a4901f4
Tabitha <tabitha@Tabithas-MacBook-Air.local>
2021-09-28 16:29:39 +0530

    loops added

---
+++ cap1_basics/06_loops.py
@@ -0,0 +1,12 @@
+# username = input("Enter username\n")
+# if(len(username) > 10):
+#     print("username is valid")
+# else:
+#     print("usrname is not valid")
+
+# Multiplication table 
+# num= int(input("enter num\n"))
+# for i in range(1,11):
+    
+#     print(str(num) + "X" +str(i)+ "="+ str(i*num))
+#     i = i +1
