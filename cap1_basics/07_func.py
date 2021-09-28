5b55c418fbb4c36f9d00fb6554b3ce0b37e23ad8
Tabitha <tabitha@Tabithas-MacBook-Air.local>
2021-09-28 21:54:51 +0530

    factorial prog

--- cap1_basics/07_func.py
+++ cap1_basics/07_func.py
@@ -1,8 +1,18 @@
+# this works as follow
+# factorial(4)
+# 4 * factorial(3)
+# 4 * 3 * factorial(2)
+# 4 * 3 * 2 * factorial(1)
+# 4 * 3 * 2 * (checks on if loop and returns 1)
+# 4 * 3 * 2 * 1
+
 def factorial(n):
     if n==0 or n==1:
         return n
     else:
-        return n* factorial(n-1)
+       return n* factorial(n-1)
+        #return n+ factorial(n-1) **Sum of n natural number
+    
 
-a = factorial(5)
+a = factorial(4)
 print(a)
